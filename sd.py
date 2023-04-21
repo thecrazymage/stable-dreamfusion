1a6a3e51539505cd13fba986859d40e07df4ac1f
“crazymage” <66633214+thecrazymage@users.noreply.github.com>
2023-04-21 14:59:12 +0300

    Update again

--- sd.py
+++ sd.py
@@ -45,8 +45,8 @@ class StableDiffusion(nn.Module):
             print(f'[INFO] using hugging face custom model key: {hf_key}')
             model_key = hf_key
         elif self.sd_version == '2.1':
-            # model_key = "stabilityai/stable-diffusion-2-1-base"
-            model_key = "./stable-diffusion-2-1-base"
+            model_key = "stabilityai/stable-diffusion-2-1-base"
+            # model_key = "./stable-diffusion-2-1-base"
         elif self.sd_version == '2.0':
             model_key = "stabilityai/stable-diffusion-2-base"
         elif self.sd_version == '1.5':
