2d3c7536acdd4ce6b61b52e2881a613107370a7c
“crazymage” <66633214+thecrazymage@users.noreply.github.com>
2023-06-06 14:39:40 +0300

    Update

--- nerf/utils.py
+++ nerf/utils.py
@@ -950,6 +950,7 @@ class Trainer(object):
 
                 # Start time for backward
                 torch.cuda.synchronize()
+                print("\Start Backward")
                 start_backward = time.time()
                 self.scaler.scale(loss).backward()
                 torch.cuda.synchronize()
